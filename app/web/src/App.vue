f80ebd0ac1a08a57e858422aaddbb4a4be3852fc
Brian Redmond <brianisrunning@gmail.com>
2018-04-26 10:01:28 -0400

    push

--- app/web/src/App.vue
+++ app/web/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="container-fluid">
-    <div class="app">
+    <div class="app" bgcolor="black">
       <router-view name="main"></router-view>
       <router-view name="footer"></router-view>
     </div>
@@ -19,7 +19,7 @@ export default {
 
 // main css
 body {
-  background-color: #ccc;
+  background-color: #C00000;
   --azure-blue: #0071c5;
   --msft-green: #4e7200;
   --msft-orange: #c72b00;
