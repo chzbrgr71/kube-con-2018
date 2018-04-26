ca569adc6f6d597b00a2868bab2ba6b364c60c23
Brian Redmond <brianisrunning@gmail.com>
2018-04-25 21:34:51 -0400

    updated

--- app/web/src/components/Footer.vue
+++ app/web/src/components/Footer.vue
@@ -14,7 +14,7 @@
       <div class="col-lg-6">
       </div>
       <div class="col-lg-12 credits">
-        Cheeseburgers Rock!
+        Hamburgers Rock!
       </div>
       <div class="col-lg-6">
       </div>
