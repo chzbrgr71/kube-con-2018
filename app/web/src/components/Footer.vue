cf26d37ae0f0228d431487057e29f2b684373fb3
Brian Redmond <brianisrunning@gmail.com>
2018-04-27 09:53:37 -0400

    change

--- app/web/src/components/Footer.vue
+++ app/web/src/components/Footer.vue
@@ -14,7 +14,7 @@
       <div class="col-lg-6">
       </div>
       <div class="col-lg-12 credits">
-        Cheeseburgers Rock!
+        Cheeseburgers Rock!!!
       </div>
       <div class="col-lg-6">
       </div>
