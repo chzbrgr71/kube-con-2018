0d137427584ec888cb123c8c65758fc5bcb33eeb
Brian Redmond <brianisrunning@gmail.com>
2018-04-27 12:15:02 -0400

    changed to new bgcolor code

--- app/web/src/App.vue
+++ app/web/src/App.vue
@@ -19,7 +19,6 @@ export default {
 
 // main css
 body {
-  background-color: #7B68EE;
   --azure-blue: #0071c5;
   --msft-green: #4e7200;
   --msft-orange: #c72b00;
@@ -69,13 +68,13 @@ html {
 
 .credits {
   font-family: "Graduate", sans-serif;
-  font-size: 24px;
+  font-size: 16px;
   color: #fff;
 }
 
 .credits-black {
   font-family: "Graduate", sans-serif;
-  font-size: 24px;
+  font-size: 16px;
   color: #CF0;
 }
 
