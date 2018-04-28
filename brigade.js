0d92169f5cc0c52ff4f1efcfa475ec8658541e1e
Brian Redmond <brianisrunning@gmail.com>
2018-04-28 16:22:22 +0200

    lame

--- brigade.js
+++ brigade.js
@@ -21,7 +21,7 @@ events.on("push", (brigadeEvent, project) => {
     var acrBuilder = new Job("job-runner-acr-builder")
     acrBuilder.storage.enabled = false
     acrBuilder.image = "briaracreu.azurecr.io/chzbrgr71/azure-cli:v2"
-    acrBuilder.tasks [
+    acrBuilder.tasks = [
         `cd /src/app/web`,
         `az login --service-principal -u ${azServicePrincipal} -p ${azClientSecret} --tenant ${azTenant}`,
         `az acr build -t ${acrImage} --build-arg BUILD_DATE="${String(today)}" --build-arg VCS_REF=${gitSHA} --build-arg IMAGE_TAG_REF=${imageTag} -f ./Dockerfile --context . -r ${acrName}`
@@ -31,7 +31,7 @@ events.on("push", (brigadeEvent, project) => {
     var helmDeploy = new Job("job-runner-helm")
     helmDeploy.storage.enabled = false
     helmDeploy.image = "briaracreu.azurecr.io/chzbrgr71/k8s-helm:v2.8.2"
-    helmDeploy.tasks [
+    helmDeploy.tasks = [
         `helm upgrade --install --reuse-values kubecon ./src/app/web/charts/kubecon-rating-web --set image=${acrServer}/${image} --set imageTag=${imageTag}`
     ]
 
