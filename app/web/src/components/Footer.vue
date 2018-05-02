095b435eaffa7699adbdd4453952854990bc79fc
Brian Redmond <brianisrunning@gmail.com>
2018-05-02 22:14:02 +0200

    fix

--- app/web/src/components/Footer.vue
+++ app/web/src/components/Footer.vue
@@ -14,7 +14,7 @@
       <div class="col-lg-6">
       </div>
       <div class="col-lg-12 credits">
-        Welcome to KubeCon 2018 @ København!
+        Welcome to KubeCon 2018 at København!
       </div>
       <div class="col-lg-6">
       </div>
