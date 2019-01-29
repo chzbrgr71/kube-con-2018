28eb49c5929f67731f1adc7459231930d0638f2d
Brian Redmond <brianisrunning@gmail.com>
2019-01-29 13:51:59 -0500

    new stuff

--- brigade.js
+++ brigade.js
@@ -18,23 +18,24 @@ events.on("push", (brigadeEvent, project) => {
 
     var acr = new Job("job-runner-acr-builder")
     acr.storage.enabled = false
-    acr.image = "briaracreu.azurecr.io/chzbrgr71/azure-cli:0.0.5"
+    acr.image = "microsoft/azure-cli:2.0.55"
     acr.tasks = [
         `cd /src/app/web`,
         `az login --service-principal -u ${azServicePrincipal} -p ${azClientSecret} --tenant ${azTenant}`,
         `az acr build -t ${acrImage} --build-arg BUILD_DATE="${String(today)}" --build-arg VCS_REF=${gitSHA} --build-arg IMAGE_TAG_REF=${imageTag} -f ./Dockerfile --context . -r ${acrName}`
     ]
 
+    /*
     var helm = new Job("job-runner-helm")
     helm.storage.enabled = false
     helm.image = "briaracreu.azurecr.io/chzbrgr71/k8s-helm:v2.8.2"
     helm.tasks = [
         `helm upgrade --install --reuse-values kubecon ./src/app/web/charts/kubecon-rating-web --set image=${acrServer}/${image} --set imageTag=${imageTag}`
     ]
-
+    */
     var pipeline = new Group()
     pipeline.add(acr)
-    pipeline.add(helm)
+    //pipeline.add(helm)
     pipeline.runEach()
 
 
@@ -43,21 +44,5 @@ events.on("push", (brigadeEvent, project) => {
 
 events.on("after", (event, project) => {
 
-    const twitter = new Job("tweet", "briaracreu.azurecr.io/chzbrgr71/twitter-t")
-    twitter.storage.enabled = false
 
-    twitter.env = {
-        OWNER: project.secrets.OWNER,
-        CONSUMER_KEY: project.secrets.CONSUMER_KEY,
-        CONSUMER_SECRET: project.secrets.CONSUMER_SECRET,
-        ACCESS_TOKEN: project.secrets.ACCESS_TOKEN,
-        ACCESS_SECRET: project.secrets.ACCESS_SECRET
-    }
-
-    twitter.tasks = [
-        "env2creds",
-        `t update "Live Tweet from Brigade at KubeCon EU 2018! brigade rørledning færdiggjort med succes"`
-    ]
-
-    //twitter.run()
 })
